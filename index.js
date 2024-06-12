e84320680958f3ee55c42c92f4ed7b3e089346dc
Admin <lmht721@gmail.com>
2024-06-12 10:47:27 +0700

    add bt

---
+++ index.js
@@ -0,0 +1,38 @@
+function upArrowPressed() {
+  let element = document.getElementById("nobita");
+  element.style.top = parseInt(element.style.top) - 10 + "px";
+}
+function downArrowPressed() {
+  let element = document.getElementById("nobita");
+  element.style.top = parseInt(element.style.top) + 10 + "px";
+}
+function leftArrowPressed() {
+  let element = document.getElementById("nobita");
+  element.style.left = parseInt(element.style.left) - 10 + "px";
+}
+function rightArrowPressed() {
+  let element = document.getElementById("nobita");
+  element.style.left = parseInt(element.style.left) + 10 + "px";
+}
+function moveSelection(event) {
+  switch (event.keyCode) {
+    case 37:
+      leftArrowPressed();
+      break;
+
+    case 38:
+      upArrowPressed();
+      break;
+
+    case 39:
+      rightArrowPressed();
+      break;
+
+    case 40:
+      downArrowPressed();
+      break;
+  }
+}
+function docReady() {
+  window.addEventListener("keydown", moveSelection);
+}
